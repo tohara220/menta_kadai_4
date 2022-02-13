9d01bed7c98dd87562f4fa268d49702b6a9cbcb7
tohara220 <tohara.220@gmail.com>
2022-02-14 08:25:10 +0900

    teishutu

--- pos-system.py
+++ pos-system.py
@@ -1,147 +1,147 @@
-import pandas as pd  # CSVファイルの読み込みに使用
-import sys           # プログラムの終了に使用
-import datetime      # レシートファイル名に使用
-import os            # ディレクトリ作成に使用
+import pandas as pd
+import datetime
+import os
 
 # 定数、変数
+now = datetime.datetime.now()
+ymdhms = f"{now:%Y%m%d%H%M%S}"
 CSV_PATH = "item_master.csv"
-master = '"001",りんご,100\n' + \
-         '"002", なし, 120\n' + \
-         '"003", みかん, 150'
-RECEIPT_FOLDER = "./receipt"
-os.makedirs(RECEIPT_FOLDER, exist_ok=True)
+RECEIPT_DIR = "./receipt"
+receipt_path = RECEIPT_DIR + f"/{ymdhms}.log"
 
 ### 商品クラス
 class Item:
-    def __init__(self, item_code, item_name, price):
-        self.item_code = item_code
-        self.item_name = item_name
-        self.price = price
-    
+    def __init__(self,item_code,item_name,price):
+        self.item_code=item_code
+        self.item_name=item_name
+        self.price=price
+        
     def get_price(self):
         return self.price
-        
+
 ### オーダークラス
 class Order:
-    def __init__(self, item_master):
-        self.item_order_list = []
-        self.item_count_list = []
-        self.item_master = item_master
-        self.set_datetime()
-    
-    def add_item_order(self, item_code, item_count):
+    def __init__(self,item_master):
+        # 商品マスタ
+        self.item_master=item_master
+        self.item_master_code_list = []
+        # オーダー
+        self.item_order_list=[]
+        self.item_order_quantity_list = []
+        self.total_price = 0
+        self.total_quantity = 0
+    def add_item_order(self,item_code):
+        '''オーダーした商品をリストに追加（商品番号のみ）'''
         self.item_order_list.append(item_code)
-        self.item_count_list.append(item_count)
-            
-    def get_item_data(self, item_code):
+        
+    def add_item_quantity(self, quantity):
+        '''オーダー数量を記録'''
+        self.item_order_quantity_list.append(quantity)
+        
+    def view_item_list(self):
+        for item in self.item_order_list:
+            print("商品コード:{}".format(item))
+    
+    def view_name_price(self, item_code):
+        '''オーダー済み商品の名前、価格を表示する'''
         for m in self.item_master:
-            if item_code == m.item_code:
+            if m.item_code == item_code:
                 return m.item_name, m.price
-    
-    def input_order(self):
-        # [課題2]コンソールから商品登録をさせる
-        print("いらっしゃいませ!")
+            
+    def view_order_list(self):
+        '''オーダーした商品の一覧と合計を表示 + レシート出力'''
+        # レシートの見出しを出力
+        make_receipt("----------\nご注文明細")
+        # 注文済み商品の商品番号と数量をループで回す
+        for code, quantity in zip(self.item_order_list, self.item_order_quantity_list):
+            # 商品番号より商品名と価格を呼び出す
+            name, price = self.view_name_price(code)
+            # コンソールに情報を表示
+            print(f"[{name} ¥{price}] {quantity}個")
+            # レシートに追記
+            make_receipt(f"[{name} ¥{price}] {quantity}個")
+            # 合計金額に加算
+            self.total_price += (int(price) * int(quantity))
+            # 合計数量に加算
+            self.total_quantity += int(quantity)
+            
+        # コンソール画面に表示
+        print(f"[合計金額:{self.total_price}, 合計数量:{self.total_quantity}]")
+        # レシートに追記
+        make_receipt(f"[合計金額:{self.total_price}, 合計数量:{self.total_quantity}]")
+        
+    def pay(self):
+        '''受け取り金額の入力とお釣りの計算 + レシート追記'''
+        receive = input(f"お支払い金額は¥{self.total_price}です。受け取り金額を入力してください。 >>")
+        oturi = int(receive) - int(self.total_price)
+        # コンソール画面に表示
+        print(f"お釣りは{oturi}です。ありがとうございました。")
+        # レシートに追記
+        make_receipt(f"[お受け取り金額:{receive},お釣り:{oturi}]\n----------")
+                
+    def order_from_console(self):
+        '''コンソール画面からオーダー登録をする'''
+        # マスタリスト（商品コードのみ）を作成する
+        for item_obj in self.item_master:
+            self.item_master_code_list.append(item_obj.item_code)
+        print("--- オーダー ---")
         while True:
-            buy_item_code = input("購入したい商品を入力してください。登録を終了する場合は0を入力してください。>>>")
-            if int(buy_item_code) != 0:
-                # マスタに存在するかチェック
-                check = self.get_item_data(str(buy_item_code).zfill(3))
-                print(check)
-                # get_item_dataの戻り値がNoneでなければ（登録されていれば）OK
-                if check != None:
-                    # リストの0番目、すなわち「商品名」が登録された旨を表示。
-                    print(f"{check[0]}が登録されました。")
-                    buy_item_count = input("個数を入力してください。 >>")
-                    # 商品登録をする
-                    self.add_item_order(str(buy_item_code).zfill(3), buy_item_count)
-                # Noneならマスタにないので元に戻る
-                else:
-                    print(f"「{buy_item_code.zfill(3)}」は商品マスタに存在しません。")
-            else:
-                print("商品登録を終了します。")
+            input_num = input("商品コードを入力してください。（'n'キーでオーダーストップ）>>")
+            if input_num == "n":
+                print("--- オーダーを終了します。 ---")
                 break
-            
-    def set_datetime(self):
-        self.datetime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-            
-    def view_order(self):
-        number = 1
-        self.sum_price = 0
-        self.sum_count = 0
-        self.receipt_name = f"receipt_{self.datetime}.log"
-        self.write_receipt("------------------------")
-        self.write_receipt("オーダー登録された商品一覧\n")
-        for item_order, item_count in zip(self.item_order_list, self.item_count_list):
-            result = self.get_item_data(item_order)
-            self.sum_price += result[1] * int(item_count)
-            self.sum_count += int(item_count)
-            receipt_data="{0}.{2}({1}) : ￥{3:,}　{4}個 = ￥{5:,}".format(number,item_order,result[0],result[1],item_count,int(result[1])*int(item_count))
-            self.write_receipt(receipt_data)
-            number += 1
-            
-        # 合計金額、個数の表示
-        self.write_receipt("------------------------")
-        self.write_receipt("合計金額:￥{:,} {}個".format(self.sum_price,self.sum_count))
-        
-    def write_receipt(self, text):
-        '''
-        レシートファイルとして書き込み
-        '''
-        print(text)
-        with open(f"{RECEIPT_FOLDER}/{self.receipt_name}", mode="a", encoding="utf-8_sig") as f:
-            f.write(text+"\n")
-            
-    def input_and_change_money(self):
-        if len(self.item_order_list)>=1:
-            while True:
-                self.money = int(input("お預かり金額を入力してください。 >>"))
-                self.change_money = self.money - self.sum_price # お釣り
-                if self.change_money >= 0:
-                    self.write_receipt("お預かり金:¥{:,}".format(self.money))
-                    self.write_receipt("お釣り:¥{:,}".format(self.change_money))
-                    break
+            ### オーダー登録する
+            if input_num in self.item_master_code_list:
+                # 商品番号
+                self.add_item_order(input_num)
+                # 数量
+                input_qt = input("続いて注文数量を入力してください。 >>")
+                if str(input_qt).isdecimal():
+                    self.add_item_quantity(input_qt)
+                    print("個数の登録も完了しました。")
                 else:
-                    print("¥{:,}不足しています。再度入力してください。".format(self.change_money))
-                    
-            print("お買い上げありがとうございました。")
-        
-    
-def regist_item_csv(path):
-    '''
-    CSVファイルの存在確認・読み込み
-    '''
-    print("------- マスタ登録開始 ---------")
-    item_master=[]
-    try:
-        item_master_df=pd.read_csv(CSV_PATH,dtype={"item_code":object}) # CSVでは先頭の0が削除されるためこれを保持するための設定
-        for item_code,item_name,price in zip(list(item_master_df["item_code"]),list(item_master_df["item_name"]),list(item_master_df["price"])):
-            item_master.append(Item(item_code,item_name,price))
-            print(f"{item_name}({item_code})")
-        print("------- マスタ登録完了 ---------")
-        return item_master
-    except:
-        print("マスタ登録が失敗しました")
-        print("------- マスタ登録完了 ---------")
-        sys.exit()
+                    print("個数が正しく入力されていません。最初からやり直してください。")
+                    # 先ほど登録してしまった商品番号を削除する
+                    del self.item_order_list[-1]
+                    continue
+            else:
+                print("商品コードが不正です。番号を再確認の上、登録してください。")
+                
+def item_master_from_csv():
+    '''CSVファイルからマスタ登録する'''
+    item_master = []
+    df = pd.read_csv(CSV_PATH)
+    print("--- 商品マスタ登録 ---")
+    for code, name, price in zip(list(df["item_code"]), list(df["item_name"]), list(df["price"])):
+        item_master.append(Item(str(code).zfill(3), name, price))
+        print(str(code).zfill(3), name, price)
+    print("--- 商品マスタ登録完了 ---")
+    return item_master
+
+def make_receipt(txt):
+    with open(receipt_path, mode="a", encoding="utf-8_sig")as f:
+        f.write(txt + "\n")
             
 ### メイン処理
 def main():
-    # CSVファイルの存在確認・データ読み込み
-    item_master=regist_item_csv(CSV_PATH) # CSVからマスタへ登録
-    order=Order(item_master) #マスタをオーダーに登録
+    # ディレクトリ作成
+    os.makedirs(RECEIPT_DIR, exist_ok=True)
+    # [課題3]CSVファイルよりマスタ登録する
+    item_master = item_master_from_csv()
+    # オーダー登録
+    order=Order(item_master)
+    # [課題2]コンソール画面よりオーダーを受け付ける
+    order.order_from_console()
     
-    # [課題4-2]ターミナルから商品登録
-    order = Order(item_master)
-    order.input_order()
-    # オーダー番号から商品情報を取得する
-    order.view_order()
-    order.input_and_change_money()
+    # # [課題1]オーダーした商品の商品名と価格を表示する
+    # for item_num in order.item_order_list:
+    #     print(order.view_name_price(item_num))
     
-    # # [課題4-1]オーダー番号から商品情報を取得する
-    # print("**[課題1] オーダー番号から商品情報を取得する**")
-    # for item_code in order.item_order_list:
-    #     print(order.get_item_data(item_code))
-    #     print(order.get_item_data(item_code))
-        
+    # [課題5] オーダー登録した商品の商品名、価格を表示し、かつ合計金額、個数を表示できるようにしてください
+    print("--- ご注文明細 ---")
+    order.view_order_list()
+    # お釣りの計算等
+    order.pay()
+    
 if __name__ == "__main__":
     main()
